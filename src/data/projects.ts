b13156cc077e7d4983309024a80eb4e3437e3c31
YusnelG <yusnelleyvag@gmail.com>
2024-01-18 22:33:09 -0500

    Change CV and Add Skills main page

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -10,29 +10,12 @@ export type Project = {
 
 const projects: Project[] = [
   {
-    title: "Project 1",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    techs: ["test", "test", "test"],
-    link: "#",
-    codelink: "#",
-    img: "/post_img.webp",
-  },
-  {
     title: "Real Time Chat App",
     description: "A simple real-time chat application with the functionality of creating private groups.",
     techs: ["Python" ,"Flask", "SocketIO", "JavaScript"],
     link: "https://yusnelg.pythonanywhere.com/",
     codelink: "https://github.com/YusnelG/python-flask-chat",
     img: "/projects/realtimechat.webp",
-  },
-  {
-    title: "Fpip",
-    description: "A pip clone created in the Rust language to make it more efficient and faster",
-    techs: ["Rust", "Fs", "http"],
-    link: "/",
-    codelink: "/",
-    img: "/projects/fpip.webp",
-    isComingSoon: true,
   },
   
 ];
---
+++ src/data/tech.ts
@@ -0,0 +1,3 @@
+const techs = ["TypeScript", "Vue 3", "Python", "Flask", "Django", "TailwindCSS", "HTML", "Node.js", "SocketIO",];
+
+export default techs;
